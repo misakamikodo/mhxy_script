fa6590ce55103fbc967ac824bb6092bc6932d170
bonelf <ccykirito@163.com>
2023-03-12 00:50:19 +0800

    update

--- mhxy_520.py
+++ mhxy_520.py
@@ -7,7 +7,7 @@ from mhxy_ghost_withshop import *
 if __name__ == '__main__':
     try:
         print("start task....")
-        fuben = Fuben(idx=1)
+        fuben = Fuben(idx=0)
         fuben.fubenPos = [
             # ("xiashi", 13, 15),
             # ("xiashi", 7, 15),
@@ -26,7 +26,7 @@ if __name__ == '__main__':
         # fuben.config['zhen'] = r'resources/small/zhen_long.png'
         fuben.fuben()
 
-        ghost = GhostWithShop(idx=1)
+        ghost = GhostWithShop(idx=0)
         ghost.maxRound = 26
         ghost.chasepos = 2
         ghost.go()
