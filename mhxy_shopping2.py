d3e45008fce20f1d18ce85d9d725da1dcbee6355
bonelf <ccykirito@163.com>
2023-03-25 13:23:51 +0800

    抢公示购买开发完成

--- mhxy_shopping2.py
+++ mhxy_shopping2.py
@@ -1,12 +1,16 @@
+import datetime
+
 from mhxy import *
 
-
+# 收纹饰 宝宝胚子
 class Shopping2:
     # 购买总商品数
-    _startTime = datetime.datetime.now()
+    _startTime = None
     #  购买时间点 没差3分钟需要至少设置一个
     _timeList = [
-        datetime.datetime(_startTime.year, _startTime.month, _startTime.day, 2, 19),
+    ]
+    __mostOldTime=None
+    _datetimeList = [
     ]
     # _total = len(_timeList)
     _total = 1
@@ -16,6 +20,23 @@ class Shopping2:
 
     def __init__(self) -> None:
         init()
+        now = datetime.datetime.now()
+        self._startTime = datetime.datetime(now.year, now.month, now.day, 12, 20)
+        # TODO
+        self._timeList = [
+            (2, 8),
+            (2, 24),
+            (0, 54),
+            (0, 6),
+            (0, 11),
+            (3, 38),
+            (3, 47),
+            (3, 18),
+        ]
+        for each in self._timeList:
+            self._datetimeList.append(self._startTime + datetime.timedelta(hours=each[0], minutes=each[1]))
+        print(self._datetimeList)
+        self.__mostOldTime = max(self._datetimeList)
         super().__init__()
 
     def openSop(self):
@@ -35,23 +56,27 @@ class Shopping2:
     def _buy(self):
         buyTab = (frame.right - relativeX2Act(5), frame.bottom - relativeY2Act(3))
         pyautogui.leftClick(buyTab[0], buyTab[1])
+        # confirmTab = (frame.left + relativeX2Act(13.5), frame.top + relativeY2Act(8.5))
         confirmTab = (frame.left + relativeX2Act(8), frame.top + relativeY2Act(14))
-        # pyautogui.leftClick(confirmTab[0], confirmTab[1])
+        pyautogui.leftClick(confirmTab[0], confirmTab[1])
 
     def _timeApproach(self):
         now = datetime.datetime.now()
         oldCount = 0
-        for time in self._timeList:
+        for time in self._datetimeList:
             # 三分钟开始刷新页面
-            sj = now + datetime.timedelta(minutes=3)
+            sj1 = now - datetime.timedelta(minutes=2)
+            sj2 = now + datetime.timedelta(minutes=1)
             # 三分钟内
-            if now < time and sj > time:
+            if sj1 < time and sj2 > time:
                 return True
             elif now > time:
                 oldCount += 1
-        if oldCount == len(self._timeList):
+            # print(time, sj1)
+        if oldCount == len(self._datetimeList):
             # 全部过期
             self._flag = False
+            print("全部过期1")
         return False
 
     class _End(Exception):
@@ -59,9 +84,15 @@ class Shopping2:
 
     def shopping2(self):
         while self._flag:
-            if self._count >= self._total:
+            if datetime.datetime.now() >= self.__mostOldTime:
+                print("全部过期2")
+                print(self.__mostOldTime)
                 self._flag = False
                 break
+            # 因为可能存在重复购买情况，所以先不考虑
+            # if self._count >= self._total:
+            #     self._flag = False
+            #     break
             while self._timeApproach():
                 # 找三次是否有商品
                 itemPic = [r'resources/shop/item_2.png']
@@ -75,13 +106,14 @@ class Shopping2:
                 if point is None:
                     self._refresh()
                 else:
-                    print("购买商品")
+                    print("购买商品", datetime.datetime.now())
                     # 如果有则购买
                     pyautogui.leftClick(point.x, point.y)
                     self._buy()
                     noMoney = pyautogui.locateOnScreen(r'resources/shop/no_money.png',
                                                        region=(frame.left, frame.top, frame.right, frame.bottom))
                     if noMoney is not None:
+                        print("没钱")
                         self._flag = False
                         raise self._End()
                     self._count += 1
--- mhxy_shopping3.py
+++ mhxy_shopping3.py
@@ -2,36 +2,41 @@ import datetime
 
 from mhxy import *
 
-now = datetime.datetime.now()
-
-
-class Shopping2:
-    _time = datetime.datetime(now.year, now.month, now.day, 2, 25)
+class Shopping3:
+    _approached = False
+    _startTime = datetime.datetime.now()
+    _time = datetime.datetime(_startTime.year, _startTime.month, _startTime.day, 13, 22)
 
     def _refresh(self):
-        # TODO
-        leftTab = (frame.left + relativeX2Act(5), frame.top + relativeY2Act(8.5))
-        pyautogui.leftClick(leftTab[0], leftTab[1])
-        leftTab = (frame.left + relativeX2Act(5), frame.top + relativeY2Act(8.5))
-        pyautogui.leftClick(leftTab[0], leftTab[1])
+        cooldown(0.2)
+        Util.leftClick(5, 7)
+        cooldown(0.2)
+        Util.leftClick(23, 18.5)
 
     def _buy(self):
+        cooldown(0.1)
         buyTab = (frame.right - relativeX2Act(5), frame.bottom - relativeY2Act(3))
         pyautogui.leftClick(buyTab[0], buyTab[1])
-        confirmTab = (frame.left + relativeX2Act(8), frame.top + relativeY2Act(14))
+        cooldown(0.1)
+        confirmTab = (frame.left + relativeX2Act(13.5), frame.top + relativeY2Act(8.5))
+        # confirmTab = (frame.left + relativeX2Act(8), frame.top + relativeY2Act(14))
         pyautogui.leftClick(confirmTab[0], confirmTab[1])
 
     def _timeApproach(self):
+        now = datetime.datetime.now()
         # 三分钟开始刷新页面
-        sj = now + datetime.timedelta(minutes=3)
-        if now < self._time and sj > self._time:
+        sj1 = now - datetime.timedelta(minutes=2)
+        sj2 = now + datetime.timedelta(minutes=1)
+        # 三分钟内
+        if sj1 < self._time and sj2 > self._time:
+            self._approached=True
             return True
         return False
 
     class _End(Exception):
         pass
 
-    def shopping2(self):
+    def shopping3(self):
         while True:
             while self._timeApproach():
                 # 找三次是否有商品
@@ -47,8 +52,10 @@ class Shopping2:
                     # 如果有则购买
                     pyautogui.leftClick(point.x, point.y)
                     self._buy()
-                    raise self._End()
-                cooldown(2)
+                    break
+                cooldown(1.5)
+            if self._approached:
+                break
             cooldown(60)
 
 # 不关注、靠搜索抢
@@ -56,4 +63,4 @@ if __name__ == '__main__':
     pyautogui.PAUSE = 0.2
     print("start task....")
     init()
-    Shopping2().shopping2()
+    Shopping3().shopping3()
