78ed05a2944ea436ac060920d32c869052030d18
bonelf <ccykirito@163.com>
2023-08-14 22:27:22 +0800

    修复bug

--- mhxy_menpai.py
+++ mhxy_menpai.py
@@ -18,26 +18,36 @@ class Menpai(MhxyScript):
         chasepos = float(conn.get('main', 'chasepos'))
         self.chaseWin[1] = 3.8 + chasepos * 2
 
+    def take_mission(self, startPos):
+        pyautogui.leftClick(startPos.x, startPos.y)
+        cooldown(0.5)
+        Util.leftClick(-4, 12.5)
+        cooldown(0.5)
+
     def do(self):
-        def start():
+
+        def start_pos():
             return pyautogui.locateCenterOnScreen(r'resources/menpai/start.png')
 
-        # 流程任务 领取任务开始
-        def do():
-            def reach():
-                return pyautogui.locateCenterOnScreen(r'resources/menpai/select.png')
+        def reach_pos():
+            return pyautogui.locateCenterOnScreen(r'resources/menpai/select.png')
 
+        # 流程任务 领取任务开始
+        def do_out_of_battle():
             # 离开战斗后点击启动
-            Util.doubleClick(self.chaseWin[0], self.chaseWin[1])
-            startPos = start()
-            reachPos = reach()
+            Util.leftClick(18, 16)
+            cooldown(0.2)
+            Util.leftClick(self.chaseWin[0], self.chaseWin[1])
+            cooldown(1)
+            startPos = start_pos()
+            reachPos = reach_pos()
             times = 0
             while reachPos is None and startPos is None and times < 10:
                 if not self._flag:
                     sys.exit(0)
-                reachPos = reach()
+                reachPos = reach_pos()
                 if reachPos is None:
-                    startPos = start()
+                    startPos = start_pos()
                 cooldown(1)
                 times += 1
                 # 新的一个战斗或完成一轮
@@ -50,28 +60,24 @@ class Menpai(MhxyScript):
             if reachPos is not None:
                 pyautogui.leftClick(reachPos.x, reachPos.y)
             elif startPos is not None:
-                pyautogui.leftClick(startPos.x, startPos.y)
-                cooldown(1)
-                Util.leftClick(-4, 12.5)
+                self.take_mission(startPos)
+                Util.leftClick(18, 16)
                 cooldown(0.2)
-                Util.doubleClick(self.chaseWin[0], self.chaseWin[1])
+                Util.leftClick(self.chaseWin[0], self.chaseWin[1])
                 while reachPos is None:
-                    reachPos = reach()
+                    reachPos = reach_pos()
                     cooldown(0.5)
                 pyautogui.leftClick(reachPos.x, reachPos.y)
 
-        startPos = start()
+        cooldown(2)
+        startPos = start_pos()
         if startPos is not None:
-            pyautogui.leftClick(startPos.x, startPos.y)
-            cooldown(1)
-            Util.leftClick(-4, 12.5)
-            cooldown(0.2)
-            Util.doubleClick(self.chaseWin[0], self.chaseWin[1])
-        escapeBattleDo(do)
+            self.take_mission(startPos)
+        escapeBattleDo(do_out_of_battle)
 
 
 # 副本 进入第一个副本为起点
 if __name__ == '__main__':
     pyautogui.PAUSE = 0.5
     print("start task....")
-    Menpai(idx=1).do()
+    Menpai(idx=0).do()
